0eefe5664d20318b27349fed4451c36070b8c0f6
maheshthippala <mahesh.cubex@gmail.com>
2018-07-27 13:41:00 +0530

    Added MVP Functionality...

--- app/src/main/java/cubex/mahesh/mvptest_july730am/model/IncExpModel.kt
+++ app/src/main/java/cubex/mahesh/mvptest_july730am/model/IncExpModel.kt
@@ -1,21 +1,47 @@
 package cubex.mahesh.mvptest_july730am.model
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import cubex.mahesh.mvptest_july730am.beans.IncExpBean
 import cubex.mahesh.mvptest_july730am.presenter.IncExpPresenterAPI
 import cubex.mahesh.mvptest_july730am.view.IncExpViewAPI
+import cubex.mahesh.mvptest_july730am.view.MainActivity
 
 class IncExpModel:IncExpPresenterAPI {
-
+        var dBase:SQLiteDatabase? = null
+        var api:IncExpViewAPI? = null
     constructor(api:IncExpViewAPI){
-        
+        this.api = api
+      var mActivity = api as MainActivity
+            dBase = mActivity.openOrCreateDatabase(
+        "incexp", Context.MODE_PRIVATE,
+                    null)
+        dBase!!.execSQL("create table if not exists incexp(_id integer primary key autoincrement,date_ varchar(20),money number,desc_ varchar(500),type varchar(50))")
     }
 
     override fun addInput(bean: IncExpBean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+            var cv = ContentValues( )
+            cv.put("date_",bean.date);
+            cv.put("money",bean.money)
+            cv.put("desc_",bean.desc)
+            cv.put("type",bean.type)
+
+ var status =    dBase!!.insert("incexp",
+         null,cv)
+   if(status==-1.toLong()){
+            api!!.addResponse("Insertion is Fail")
+   }else{
+       api!!.addResponse("Insertion is Success")
+   }
     }
 
     override fun viewInput(type: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+      var c =   dBase!!.query("incexp",null,
+              "type=?",
+              arrayOf(type),null,null,null)
+        api!!.viewResponse(c)
     }
 
 }
--- app/src/main/java/cubex/mahesh/mvptest_july730am/view/MainActivity.kt
+++ app/src/main/java/cubex/mahesh/mvptest_july730am/view/MainActivity.kt
@@ -3,19 +3,47 @@ package cubex.mahesh.mvptest_july730am.view
 import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import cubex.mahesh.mvptest_july730am.R
+import cubex.mahesh.mvptest_july730am.beans.IncExpBean
+import cubex.mahesh.mvptest_july730am.model.IncExpModel
+import cubex.mahesh.mvptest_july730am.presenter.IncExpPresenterAPI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),IncExpViewAPI {
     override fun addResponse(msg: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this,msg,
+                Toast.LENGTH_LONG).show()
     }
 
     override fun viewResponse(c: Cursor) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        var myadapter = SimpleCursorAdapter(this,
+ R.layout.indiview,c,
+                arrayOf("date_","money","desc_","type"),
+ intArrayOf(R.id.date,R.id.money,R.id.desc,R.id.type),
+                0)
+    lview.adapter = myadapter
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        add.setOnClickListener({
+            var bean = IncExpBean(et1.text.toString(),
+                    et2.text.toString().toInt(),et3.text.toString(),
+                    sp1.selectedItem.toString())
+
+            var api:IncExpPresenterAPI =
+                    IncExpModel(this@MainActivity )
+
+            api.addInput(bean)
+        })
+        view.setOnClickListener({
+            var api:IncExpPresenterAPI =
+                    IncExpModel(this@MainActivity )
+            api.viewInput(sp1.selectedItem.toString())
+        })
     }
 }
